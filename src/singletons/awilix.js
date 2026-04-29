a83467d50def8c2ccc760b4d00aa8e95f6bb6d13
Abhishek <itz.abk007@gmail.com>
2026-04-29 18:11:06 +0530

    feat: implement awilix container for dependency injection and service management

---
+++ src/singletons/awilix.js
@@ -0,0 +1,77 @@
+import path from 'node:path';
+
+import { db } from './mongoDb';
+
+import { defaultRedisClient } from './redis';
+
+import { asClass, asValue, createContainer, InjectionMode, Lifetime } from 'awilix';
+
+import Logger from '../helpers/pino';
+
+const logger = new Logger();
+
+const awilixOpts = {
+  formatName: (name, descriptor) => {
+    const cleanPath = path.normalize(descriptor.path);
+    const splat = cleanPath.split(path.sep);
+    const fileName = splat[splat.length - 1];
+    const parentName = splat[splat.length - 2];
+    const rootName = splat[splat.length - 3];
+    const awilixInjectorName =
+      parentName + rootName.charAt(0) + rootName.slice(1, splat.length - 1);
+    return awilixInjectorName;
+  },
+  resolverOptions: {
+    lifetime: Lifetime.SINGLETON,
+    register: asClass,
+  },
+};
+
+async function initAwilixContainer() {
+  logger.trace(
+    {
+      file: 'mainThread',
+      service: 'awilix',
+      method: 'initAwilixContainer',
+    },
+    'Started initializing awilix container'
+  );
+  try {
+    const container = createContainer({
+      strict: true,
+      injectionMode: InjectionMode.PROXY,
+    });
+
+    container.register({
+      mongooseConnection: asValue(db),
+      redisConnection: asValue(defaultRedisClient),
+      logger: asValue(logger),
+    });
+
+    await container.loadModules(['../services/*/index.js', '../models/*/index.js'], awilixOpts);
+
+    logger.info(
+      {
+        file: 'mainThread',
+        service: 'awilix',
+        method: 'initAwilixContainer',
+      },
+      'Awilix container created successfully'
+    );
+
+    return container;
+  } catch (err) {
+    logger.error(
+      {
+        file: 'mainThread',
+        service: 'awilix',
+        method: 'initAwilixContainer',
+        meta: { err },
+      },
+      'Error occured during awilix container creation'
+    );
+    throw err;
+  }
+}
+
+export const awilixContainer = await initAwilixContainer();
